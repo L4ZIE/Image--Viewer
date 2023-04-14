19f39b58c93990cfa6356282919911506d7fa5ef
Bruger <davidwymon@yahoo.com>
2023-04-15 01:58:22 +0200

    slideshow added

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -3,6 +3,9 @@ package dk.easv;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -11,17 +14,31 @@ import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class ImageViewerWindowController
 {
     private final List<Image> images = new ArrayList<>();
     private int currentImageIndex = 0;
+    private Timeline timeline;
+    private Thread slideshowThread;
 
     @FXML
     Parent root;
 
     @FXML
     private ImageView imageView;
+
+
+    private void handleAutoSlideshow(ActionEvent event) {
+        currentImageIndex = (currentImageIndex + 1) % images.size();
+        displayImage();
+    }
+
+    private void initialize() {
+        timeline = new Timeline(new KeyFrame(Duration.seconds(3), this::handleAutoSlideshow));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+    }
 
     @FXML
     private void handleBtnLoadAction()
@@ -65,9 +82,33 @@ public class ImageViewerWindowController
 
     private void displayImage()
     {
+
         if (!images.isEmpty())
         {
             imageView.setImage(images.get(currentImageIndex));
         }
     }
+    @FXML
+    private void handleBtnStartSlideshowAction() {
+        slideshowThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                handleAutoSlideshow(null);
+            }
+        });
+        slideshowThread.start();
+    }
+
+    @FXML
+    private void handleBtnStopSlideshowAction() {
+        if (slideshowThread != null) {
+            slideshowThread.interrupt();
+        }
+    }
+
+
 }
